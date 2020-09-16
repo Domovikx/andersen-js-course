834c402b66b0d05915eceaf46d1c5e162075af6d
domovikx <domovikx@gmail.com>
2020-09-16 14:09:05 +0300

    chore: add variants on promise

---
+++ src/asynchronous-programming/task_004_promise.js
@@ -0,0 +1,30 @@
+/**
+ * 4 Написать функцию в которой будет запрос URL_1,
+ * из ответа получить поле getUserData
+ * если значение getUserData - true, получить данные с URL_2 и вывести хи в консоль
+ */
+
+const URL_1 = 'http://www.json-generator.com/api/json/get/cfQCylRjuG';
+const URL_2 = 'http://www.json-generator.com/api/json/get/cfVGucaXPC';
+
+function query(URL) {
+  const promise = fetch(URL).then(response => response.json());
+  return promise;
+}
+
+function getUserData() {
+  query(URL_1)
+    .then(res => res)
+    .then(obj => {
+      if (obj.getUsersData === true) {
+        query(URL_2).then(res => {
+          console.log('res :>> ', res);
+        });
+      }
+    });
+}
+
+// check
+getUserData();
+
+export { getUserData };
---
+++ src/asynchronous-programming/task_005_promise.js
@@ -0,0 +1,55 @@
+/**
+ * 5 Написать функцию в которой получить данные по URLS
+ * реализовать два варианте: параллельной и последовательной загрузки
+ */
+
+// helper function
+function query(URL) {
+  const result = fetch(URL)
+    .then(response => response.json())
+    .then(
+      result => {
+        return result;
+      },
+      error => {
+        return { error: error.message.toString() };
+      }
+    );
+  return result;
+}
+
+const URLS = [
+  'error',
+  'http://www.json-generator.com/api/json/get/cevhxOsZnS',
+  'http://www.json-generator.com/api/json/get/cguaPsRxAi',
+  'http://www.json-generator.com/api/json/get/cfDZdmxnDm',
+  'error',
+  'http://www.json-generator.com/api/json/get/cfkrfOjrfS',
+  'http://www.json-generator.com/api/json/get/ceQMMKpidK',
+  'error',
+];
+
+function parallelLoading() {
+  Promise.all(
+    URLS.map(URL => {
+      const result = query(URL);
+      return result;
+    })
+  ).then(results => {
+    console.log('>>> parallelLoading() - results: ', results);
+  });
+}
+parallelLoading();
+
+function consistentLoading() {
+  const results = [];
+  URLS.forEach(URL => {
+    query(URL).then(result => {
+      results.push(result);
+    });
+  });
+  console.log('>>> consistentLoading() - results: ', results);
+}
+consistentLoading();
+
+export { parallelLoading, consistentLoading };
